d9dfb2429c08b0744795a5a22efb9bbac1cb7c74
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-31 21:13:52 +0530

    first commit

---
+++ p1.c
@@ -0,0 +1,13 @@
+// Program for output of an array
+#include <stdio.h>
+
+int main() {
+  
+  int a[10]={1,2,3,4,5,6,7,8,9,10};
+  int i;
+  for (i = 0; i < 10; i++) {
+    printf("elements of array are %d\n",a[i]);
+  }
+ 
+  return 0;
+} 
