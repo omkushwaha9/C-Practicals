b8141382e1d3d16aeb5f1f2b38c6911d05fdf3e1
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-31 22:22:04 +0530

    practicals

---
+++ p0.c
@@ -0,0 +1,7 @@
+// write a Hellow World Program
+#include <stdio.h>
+
+int main() {
+  printf("Hello World!");
+  return 0;
+}
--- p1.c
+++ p1.c
@@ -5,7 +5,7 @@ int main() {
   
   int a[10]={1,2,3,4,5,6,7,8,9,10};
   int i;
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < 7; i++) {
     printf("elements of array are %d\n",a[i]);
   }
  
