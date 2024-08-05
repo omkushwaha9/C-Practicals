ded8cf26b6bf7eec809e7486fdfe4e36758105a6
omkushwaha9 <omkushwaha2005@gmail.com>
2024-08-05 12:07:49 +0530

    Extra Tutorial Practice

---
+++ ExtraPractice/p1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void myfunction(){
+printf("CALSS IT-B\n");
+}
+void my2function(){
+printf("Student name OM and JAY are, MISSING! ");
+}
+
+int main(){
+myfunction();
+my2function();
+return 0;
+}
