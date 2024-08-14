85b34db753aac5860c81ca1948e6783d8f5e7ce4
omkushwaha9 <omkushwaha2005@gmail.com>
2024-08-14 17:27:41 +0530

    PRacticals

--- p5.c
+++ p5.c
@@ -1,4 +1,16 @@
+
 #include <stdio.h>
 int main(){
-    
+    int a[3][3][3],i,j,k;
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            for(k=0; k<3; k++)
+            {
+                scanf("%d", &a[i][j][k]);
+                printf("Enter the Value of", &a[i][j][k]);
+            }
+        }
+    }
 }
