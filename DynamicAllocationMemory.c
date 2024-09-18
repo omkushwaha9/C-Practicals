0306f152d4ae780afea5b8a0b32b1d9f05033338
omkushwaha9 <omkushwaha2005@gmail.com>
2024-09-18 22:20:43 +0530

    Formation

--- DynamicAllocationMemory.c
+++ DynamicAllocationMemory.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  int *students;
-  int numStudents = 12;
-  students = calloc(numStudents, sizeof(*students));
-  printf("%d", numStudents * sizeof(*students)); // 48 bytes
-  
-  return 0;
+int main()
+{
+    int *students;
+    int numStudents = 12;
+    students = calloc(numStudents, sizeof(*students));
+    printf("%d", numStudents * sizeof(*students)); // 48 bytes
+
+    return 0;
 }
