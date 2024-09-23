27b41e913e6bd3f42ccbf2ba815c1204eb03cd93
omkushwaha9 <omkushwaha2005@gmail.com>
2024-09-23 12:52:45 +0530

    Formation

--- Fibonacci_Recursion.c
+++ Fibonacci_Recursion.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
-int fibonacci(int n) {
-    if (n <= 1) {
+int fibonacci(int n)
+{
+    if (n <= 1)
+    {
         return n;
     }
     return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int main() {
+int main()
+{
     int num, i;
 
     printf("Enter the number of terms: ");
     scanf("%d", &num);
 
     printf("Fibonacci Series: ");
-    for (i = 0; i < num; i++) {
+    for (i = 0; i < num; i++)
+    {
         printf("%d ", fibonacci(i));
     }
     printf("\n");
